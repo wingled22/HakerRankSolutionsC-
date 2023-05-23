6cc17cc998bfd9d3d28a13d5c14c77cba7a433dd
wingled22 <pelayowindel@gmail.com>
2023-05-23 16:33:55 +0800

    added minmaxSum

--- Program.cs
+++ Program.cs
@@ -7,7 +7,15 @@ namespace consoleApp1{
         
         static void Main(string[] args)
         {
-            
+            MiniMaxSum mms = new MiniMaxSum();
+            List<int> a = new List<int> (){
+                {256741038},
+                {623958417},
+                {467905213},
+                {714532089},
+                {938071625},
+            };
+            mms.getMaxSum(a);
         }
 
     }
---
+++ algo/MiniMaxSum.cs
@@ -0,0 +1,31 @@
+namespace consoleApp1.algo
+{
+    public class MiniMaxSum
+    {
+        public void getMaxSum(List<int> arr){
+
+            List<ulong> resultList = new List<ulong>();
+            var skipIndex = 0;
+            var len = arr.Count();
+            ulong min, max;
+
+            for (int i = 0; i < len; i++)
+            {
+                ulong tempSum = 0;
+                for (int j = 0; j < len; j++)
+                {
+                    if(skipIndex != j){
+                        tempSum = tempSum + (ulong)arr[j];
+                    }
+
+                }
+
+                resultList.Add(tempSum);
+                skipIndex++;
+            }
+            min = resultList.Min();
+            max = resultList.Max();
+            Console.WriteLine(string.Format("{0} {1}", min, max));
+        }
+    }
+}
