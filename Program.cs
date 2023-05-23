96d61fd11bea0c4d653a4f4a2aba2361729864f4
wingled22 <pelayowindel@gmail.com>
2023-05-23 14:15:16 +0800

    added some files

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using consoleApp1.algo;
+
+namespace consoleApp1{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+        }
+
+    }
+}
